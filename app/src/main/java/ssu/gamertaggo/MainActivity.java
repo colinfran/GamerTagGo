3e0ea0b934bfe4d48ef4bee1007cb1a571ed31cf
Colin <colin.franceschini@gmail.com>
2016-03-31 22:11:35 -0700

    main menu template working

--- app/src/main/java/ssu/gamertaggo/MainActivity.java
+++ app/src/main/java/ssu/gamertaggo/MainActivity.java
@@ -1,6 +1,78 @@
+//package ssu.gamertaggo;
+//
+//import android.app.Activity;
+//import android.content.Intent;
+//import android.view.View;
+//import android.widget.Button;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.os.Bundle;
+//import com.parse.*;
+//
+//
+///**
+// * Created by colinfranceschini on 3/24/16.
+// */
+//
+//// THIS MAIN ACTIVITY FILE CONTAINS CODE THAT ...
+//
+//public class MainActivity extends Activity {
+//
+//    Button logout;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        logout=(Button)findViewById(R.id.logoutbutton);
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ParseUser.logOut();
+//                back_to_sign_up_or_login();
+//            }
+//        });
+//        ParseUser cuser=ParseUser.getCurrentUser();
+//        if (cuser==null)
+//        {
+//            back_to_sign_up_or_login();
+//        }
+//
+//    }
+//
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.my, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            ParseUser.getCurrentUser().logOut();
+//            startActivity(new Intent(MainActivity.this, DispatchActivity.class));
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    void back_to_sign_up_or_login(){
+//        Intent intent=new Intent(MainActivity.this,SignUpOrLoginActivity.class);
+//        startActivity(intent);
+//    }
+//
+//}
+
 package ssu.gamertaggo;
 
-import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
@@ -9,14 +81,19 @@ import android.view.MenuItem;
 import android.os.Bundle;
 import com.parse.*;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-/**
- * Created by colinfranceschini on 3/24/16.
- */
 
-// THIS MAIN ACTIVITY FILE CONTAINS CODE THAT ...
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     Button logout;
 
@@ -25,39 +102,90 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logout=(Button)findViewById(R.id.logoutbutton);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParseUser.logOut();
-                back_to_sign_up_or_login();
-            }
-        });
-        ParseUser cuser=ParseUser.getCurrentUser();
-        if (cuser==null)
-        {
-            back_to_sign_up_or_login();
-        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.MainMenuLogout) {
+            ParseUser.logOut();
+            back_to_sign_up_or_login();
+            // Set up a progress dialog
+            final ProgressDialog dlg = new ProgressDialog(MainActivity.this);
+            dlg.setTitle("Please wait.");
+            dlg.setMessage("Logging out.  Please wait.");
+            dlg.show();
+
+            ParseUser cuser=ParseUser.getCurrentUser();
+            if (cuser==null)
+            {
+                back_to_sign_up_or_login();
+            }
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
         if (id == R.id.action_settings) {
-            ParseUser.getCurrentUser().logOut();
-            startActivity(new Intent(MainActivity.this, DispatchActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -67,4 +195,6 @@ public class MainActivity extends Activity {
         Intent intent=new Intent(MainActivity.this,SignUpOrLoginActivity.class);
         startActivity(intent);
     }
+
+
 }
